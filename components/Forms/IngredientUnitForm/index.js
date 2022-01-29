f097082081f1d53cb778e3052735eed1c5d80216
konstantin <k@k.cz>
2022-01-29 17:35:23 +0100

    [ADD] Ingredients counter

--- components/Forms/IngredientUnitForm/index.js
+++ components/Forms/IngredientUnitForm/index.js
@@ -3,12 +3,17 @@ import { PERSON_LIST } from '../../../utils/constants';
 
 function IngredientUnitForm({ defaultValue, onChange }) {
   return (
-    <div className="row my-2">
+    <div className="row my-2 d-flex align-items-center">
       <div className="col-6">
-        <span>For:</span>
+        <label htmlFor="ingredients-count">For:</label>
       </div>
       <div className="col-6">
-        <select defaultValue={defaultValue} onChange={onChange} className="form-select">
+        <select
+          id="ingredients-count"
+          defaultValue={defaultValue}
+          onChange={onChange}
+          className="form-select"
+        >
           {PERSON_LIST.map(({ label, value }) => (
             <option key={label} value={value}>
               {label}
